bb3e7ed9480672177af53c37e939f916ccc36ed8
Aleksandra <241535@student.pwr.edu.pl>
2020-05-26 15:34:40 +0200

    Łapanie obiektów

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,39 +4,57 @@ import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Node;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import javafx.scene.paint.Color;
 
-import java.util.ArrayList;
+import java.util.*;
 
 
 public class Main extends Application {
 
     private double newX = 0;
     private ArrayList<Circle> drop = new ArrayList<Circle>();
-
     private Rectangle r;
     private Pane root;
-
+    private Shapes shapes = new Shapes();
+    private Label label;
+    private Label labelCount;
+    private int counter = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         AnimationTimer timer;
-        double falling = 3000;
+        double falling = 4000; //określenie częstotliwości spadania (ms)
 
         root = new Pane();
-        r = rectangle();
+        Scene scene = new Scene(root, 800, 600);
+        scene.setFill(Color.GRAY);
+
+        r = shapes.rectangle();
         root.getChildren().add(r);
 
-        Scene scene = new Scene(root, 600, 600);
+        label = new Label("Counter: ");
+        label.setFont(new Font("Arial", 30));
+        label.setTranslateX(610);
+        label.setTranslateY(100);
+        root.getChildren().add(label);
 
+        labelCount = new Label("0");
+        labelCount.setFont(new Font("Arial", 30));
+        labelCount.setTranslateX(730);
+        labelCount.setTranslateY(100);
+        root.getChildren().add(labelCount);
+
+        // poruszanie się koszyka
         scene.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.RIGHT) {
                 if (newX != 264) {
@@ -52,9 +70,10 @@ public class Main extends Application {
 
         });
 
+
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(falling), event -> {
             //speed += falling / 10000;
-            drop.add(circle());
+            drop.add(shapes.circle());
             root.getChildren().add((drop.get(drop.size() - 1)));
         }));
 
@@ -72,52 +91,29 @@ public class Main extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
 
-    private Circle circle() {
-        Circle circle = new Circle();
-        circle.setLayoutX(rand());
-        circle.setFill(Color.BLUE);
-        circle.setLayoutY(1);
-        circle.setRadius(6);
-        return circle;
-    }
-
-    private Rectangle rectangle() {
-        Rectangle r = new Rectangle();
-        r.setX(265);
-        r.setY(560);
-        r.setWidth(70);
-        r.setHeight(40);
-        r.setArcWidth(20);
-        r.setArcHeight(20);
-        return r;
-    }
-
-    private int rand() {
-        int min = 0;
-        int max = 600;
-        return (int) (Math.random() * max + min);
     }
 
     private void gameUpdate() {
 
-        //r.getLayoutX(newX);
-        double speed = 1;
-
+        double speed = 1; //szybkość spadania
+        //System.out.println(root.getLayoutX());
         for (int i = 0; i < drop.size(); i++) {
-            (drop.get(i)).setLayoutY(( drop.get(i)).getLayoutY() + speed + (drop.get(i)).getLayoutY() / 200);
+            (drop.get(i)).setLayoutY(( drop.get(i)).getLayoutY() + speed + (drop.get(i)).getLayoutY() / 600);
+
+            //System.out.println(drop.get(i).getLayoutY());
+            if ((drop.get(i).getLayoutY() > root.getLayoutY() + r.getY())
+                    && drop.get(i).getLayoutX() > (newX + 264) && drop.get(i).getLayoutX() < (newX + 264 + 70)) {
+                if (drop.get(i).getFill() == Color.GREEN) {
+                    counter++;
+                    labelCount.setText(Integer.toString(counter));
+                }
+                root.getChildren().remove((drop.get(i)));
+                drop.remove(i);
 
 
-            /*if((((Circle) drop.get(i)).getLayoutX() > x && ((Circle) drop.get(i)).getLayoutX() < x + 70) &&
-                    ((Circle) drop.get(i)).getLayoutY() >= 400  ) {
-                root.getChildren().remove(((Circle) drop.get(i)));
-                drop.remove(i);
-            }*/
-           /* if(r.intersects(((Circle) drop.get(i)).getBoundsInLocal())) {
-                root.getChildren().remove(((Circle) drop.get(i)));
-                drop.remove(i);
-            }*/
+            }
+
         }
     }
 
